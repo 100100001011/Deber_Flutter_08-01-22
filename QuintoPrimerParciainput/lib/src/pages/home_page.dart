ff7d6293caf6b748076f3a9bfe87b5868b0f7257
CrisAlejo23 <89047686+CrisAlejo23@users.noreply.github.com>
2022-01-12 13:04:47 -0500

    deber

--- QuintoPrimerParciainput/lib/src/pages/home_page.dart
+++ QuintoPrimerParciainput/lib/src/pages/home_page.dart
@@ -11,49 +11,45 @@ class HomePage extends StatefulWidget {
 class _Contador extends State<HomePage> {
   int contador = 0;
   Bloc_1 obj_bloc = Bloc_1();
-  
-  
+
   TextEditingController cont_text = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<int>(
-      stream: obj_bloc.sale_Stream,
-      initialData: 0,
-      builder: (context, snapshot) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Text('App de conteo'),
-          ),
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Número de clicks',
-                  style: TextStyle(fontSize: 28),
-                ),
-                Text(
-                  //'$contador',
-                  '${snapshot.data}',
-                  style: TextStyle(fontSize: 30),
-                ),
-                TextField(
-                  controller: cont_text,
+        //stream: obj_bloc.sale_Stream,
+        initialData: 0,
+        builder: (context, snapshot) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('App de conteo'),
+            ),
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Número de clicks',
+                    style: TextStyle(fontSize: 28),
+                  ),
+                  Text(
+                    '$contador',
+                    //'${snapshot.data}',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                  TextField(
+                    controller: cont_text,
                     decoration: const InputDecoration(
                       hintText: 'Número de clicks',
                       contentPadding: const EdgeInsets.all(10.0),
-                    
                     ),
-                    
-                    ),
-              ],
+                  ),
+                ],
+              ),
             ),
-          ),
-          floatingActionButton: _botones(),
-        );
-      }
-    );
+            floatingActionButton: _botones(),
+          );
+        });
   }
 
   Widget _botones() {
@@ -73,35 +69,31 @@ class _Contador extends State<HomePage> {
     );
   }
 
-  void dispose(){
+  void dispose() {
     obj_bloc.dispose();
   }
 
   void aumentar() {
-    
     setState(() => {
-      //contador++,
-      //cont_text.text= contador.toString()
-      obj_bloc.ingresa_Stream.add(IncrementaContador())
-      });
-    
+          contador++,
+          cont_text.text = contador.toString()
+          //obj_bloc.ingresa_Stream.add(IncrementaContador())
+        });
   }
 
   void disminuir() {
     setState(() => {
-      //contador--,
-    //cont_text.text= contador.toString()
-    obj_bloc.ingresa_Stream.add(ReducirContador())
-    });
-    
+          contador--,
+          cont_text.text = contador.toString()
+          //obj_bloc.ingresa_Stream.add(ReducirContador())
+        });
   }
 
   void cero() {
     setState(() => {
-      obj_bloc.ingresa_Stream.add(EncerarContador())
-      //contador = 0,
-    //cont_text.text= '0'
-    });
-    
+          //obj_bloc.ingresa_Stream.add(EncerarContador())
+          contador = 0,
+          cont_text.text = '0'
+        });
   }
 }
